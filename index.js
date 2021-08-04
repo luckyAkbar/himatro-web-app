5ba9d3074ebc8b09105aa77c51f6504b08b7739d
luckyAkbar <luckyakbar1509@gmail.com>
2021-08-04 13:43:51 +0700

    change port setting to match with .env file

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+require('dotenv').config()
 const { app } = require('./src/app')
 
-app.listen(4000, () => {
-  console.log('Server estabilised on port 4000')
+app.listen(process.env.SERVER_PORT, () => {
+  console.log('Server estabilised on port', process.env.SERVER_PORT)
 })
 
 app.on('exit', () => {
+  app.close()
+})
+
+app.on('uncaughtException', () => {
+  console.log('app closed unexpectedly')
   app.close()
 })
