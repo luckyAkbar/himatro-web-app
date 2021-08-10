e116e4b452a72b92227bf7becd08305c6a143440
luckyAkbar <luckyakbar1509@gmail.com>
2021-08-10 22:51:44 +0700

    adding several event listener to app

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 require('dotenv').config()
 const { app } = require('./src/app')
 
-app.listen(process.env.SERVER_PORT, () => {
-  console.log('Server estabilised on port', process.env.SERVER_PORT)
+app.on('exit', () => {
+  console.log('app closed unexpectedly0')
+  app.close()
 })
 
-app.on('exit', () => {
+app.on('error', () => {
+  console.log('app closed unexpectedly1')
   app.close()
 })
 
 app.on('uncaughtException', () => {
-  console.log('app closed unexpectedly')
+  console.log('app closed unexpectedly2')
   app.close()
+})
+
+process.on('EADDRINUSE', () => {
+  console.log('Restarting due to address error')
+  process.exit(-1)
+})
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log('Server estabilised on port', process.env.SERVER_PORT)
 })
