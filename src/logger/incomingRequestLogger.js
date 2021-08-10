8ea1dbc9312ffa740a6c3473b90b3f140aefeae0
luckyAkbar <luckyakbar1509@gmail.com>
2021-08-10 23:01:46 +0700

    install new package: generate-unique-id, csv-load-sync, axios [now just for testing only]

--- src/logger/incomingRequestLogger.js
+++
@@ -1,7 +0,0 @@
-const incomingRequestLogger = (req, res, next) => {
-  const time = Date()
-  console.log(req.protocol, req.method, req.originalUrl, time.toString().slice(0, 33))
-  next()
-}
-
-module.exports = { incomingRequestLogger }
