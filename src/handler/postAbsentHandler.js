4e7d7bd9cf9f1daf83ce24a7e1b17d792fb1ea4c
luckyAkbar <luckyakbar1509@gmail.com>
2021-08-10 23:05:58 +0700

    create new features: init new absent record, update absent record, and several error handling in validator.js

---
+++ src/handler/postAbsentHandler.js
@@ -0,0 +1,100 @@
+const { referensiIdGenerator } = require('../util/referensiIdGenerator')
+const { createNewAbsent } = require('./createNewAbsent')
+const { postAbsentDataValidator, validateAbsentRefData } = require('./validator');
+const { absentFiller } = require('./absentFiller')
+const { testQuery } = require('../../db/connection')
+
+const postAbsentHandler = async (req, res) => {
+  if (req.query.mode === 'create') {
+    try {
+      const validate = await absentRefValidator(req)
+
+      if (!validate) {
+        res.status(400).json({error: "Data Kegiatan / Rapat Invalid"})
+        return
+      }
+      console.log('harusnya ini dulu');
+    } catch (e) {
+      console.log('cannot validate absent ref data', e)
+    }
+
+    try {
+      console.log('apalagi ini');
+      await createNewAbsentRecord(req, res)
+      console.log('bukan ini');
+    } catch (e) {
+      console.log('cannot create new absent record', e)
+    }
+  }
+
+  if (req.query.mode === 'post') {
+    if (!absentDataValidator(req)) {  
+      res.status(400).json({ error: "Data Absent Invalid" })
+      return
+    }
+
+    try {
+      await absentFiller(req, res)
+    } catch(e) {
+      console.log(e);
+    }
+  }
+}
+
+const absentDataValidator = (req) => {
+  const { refId, npm, nama, keterangan } = req.body
+
+  return postAbsentDataValidator(refId, npm, nama, keterangan)
+}
+
+const absentRefValidator = async (req) => {
+  const { namaKegiatan, tanggalPelaksanaan, tanggalBerakhir } = req.body
+
+  if (!validateAbsentRefData(namaKegiatan, tanggalPelaksanaan, tanggalBerakhir)) {
+    return false
+  }
+
+  return true
+}
+
+const createNewAbsentRecord = async (req, res) => {
+  const { ref } = req.query
+
+  if (ref === 'keg') {
+    //look at the bottom
+    const referensiId = referensiIdGenerator(ref)
+
+    try {
+      await createNewAbsent(res, referensiId)
+      res.json({ refId: `${referensiId}` })
+    } catch(e) {
+      res.status(500).json({ error: "Server Error" })
+    }
+    return
+  }
+
+  if (ref === 'rap') {
+    res.status(501).json({ error: "Not Implemented" })
+    return
+  }
+
+  if (ref === undefined) {
+    res.status(400).json({ error: "Malformed Request" })
+    return
+  }
+
+  res.status(400).json({ error: "Malformed Request "})
+  return
+}
+
+module.exports = { postAbsentHandler }
+
+/*
+let query = `SELECT * FROM kegiatan WHERE nama_kegiatan = '${namaKegiatan}' AND tanggal_pelaksanaan = '${tanggalPelaksanaan}'::date`
+
+const { rowCount } = await testQuery(query)
+
+if (rowCount === 0) {
+
+}
+*/
