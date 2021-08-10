100d9296e0c4c09dc36646121b893b25923de4cf
luckyAkbar <luckyakbar1509@gmail.com>
2021-08-10 22:46:27 +0700

    change query method to handle init new absent record, and add logging

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,4 @@
 require('dotenv').config()
-//console.log(typeof(process.env.MAX_CONNECTION))
 const { Pool } = require('pg')
 
 const config = {
@@ -7,7 +6,7 @@ const config = {
   idleTimeoutMillis: parseInt(process.env.IDLE_TIMEOUT_MILLISEC),
   max: parseInt(process.env.MAX_CONNECTION),
 }
-
+/*
 const pool = new Pool(config)
 
 pool.on('error', (err, client) => {
@@ -18,7 +17,49 @@ pool.on('error', (err, client) => {
 pool.on('connect', () => {
   console.log('Connected to the Database [/db/koneksi.js]')
 })
+*/
+const newPool = new Pool(config)
+
+newPool.on('error', (err, client) => {
+  console.log('Unexpected error from client [/db/koneksi.js]')
+  //process.exit(-1)
+});
+
+newPool.on('connect', () => {
+  console.log('Connected to the Database [/db/koneksi.js]')
+})
+
+newPool.on('remove', () => {
+  console.log('client removed')
+})
+
+newPool.on('acquire', () => {
+  console.log('client acquired')
+})
+
+newPool.on('ECONNREFUSED', () => {
+  console.log('Connection refused')
+})
+
+
+async function testQuery(query, params) {
+  try {
+    const start = Date.now()
+    const client = await newPool.connect()
+    const res = await client.query(query, params)
+
+    client.release(true)
+
+    const duration = Date.now() - start
+    console.log('executed query', query, res.rows, duration, 'ms');
+
+    return res
+  } catch(e) {
+    console.log('err connection.js', e)
+  }
+}
 
 module.exports = {
-  query: (text, params) => pool.query(text, params),
+  //query: (text, params) => pool.query(text, params),
+  testQuery
 }
