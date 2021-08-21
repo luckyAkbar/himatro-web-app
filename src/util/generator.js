26873c21a9be4d9a0c78db602ca9cc306b66e98d
luckyAkbar <luckyakbar1509@gmail.com>
2021-08-21 20:21:33 +0700

    able to generate referensiId, image name, image code, etc

---
+++ src/util/generator.js
@@ -0,0 +1,47 @@
+const generateUniqueId = require('generate-unique-id')
+
+const excludedSymbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '-', '=', '[', ']', '{', '}', ';', ':', `"`, '|', ',', '.', '<', '>', '?', ']', '+']
+
+const referensiIdGenerator = (option) => {
+  if (option === 'keg') {
+    return `keg${generateUniqueId({
+      length: 7,
+      excludeSymbols: excludedSymbols,
+      useLetters: false
+    })}`
+  }
+}
+
+const sdmUIDGenerator = () => {
+  return `sdm${generateUniqueId({
+    length: 7,
+    excludeSymbols: excludedSymbols,
+    useNumbers: true,
+    useLetters: false
+  })}`
+}
+
+const uploadedImageNameGenerator = () => {
+  return `${generateUniqueId({
+    length: 15,
+    excludeSymbols: excludedSymbols,
+    useLetters: true,
+    useNumbers: false
+  })}`
+}
+
+const imageIdGenerator = () => {
+  return `${generateUniqueId({
+    length: 10,
+    excludeSymbols: excludedSymbols,
+    useLetters: true,
+    useNumbers: true
+  })}`
+}
+
+module.exports = {
+  referensiIdGenerator,
+  uploadedImageNameGenerator,
+  sdmUIDGenerator,
+  imageIdGenerator
+ }
