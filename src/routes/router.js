d92e12f9319c3d96306f9f3896a39910f9711d92
luckyAkbar <luckyakbar1509@gmail.com>
2021-08-21 20:08:05 +0700

    removing 'all' method handler in routes (it's useless) and add new route about kaderisasi, sdm and absen sdm.

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,7 +1,11 @@
 const router = require('express').Router()
-//const db = require('../../db/connection')
 const { getAbsentHandler } = require('../handler/getAbsentHandler')
 const { postAbsentHandler } = require('../handler/postAbsentHandler')
+const { uploadAbsentSdmHandler } = require('../handler/postUploadAbsentSdmHandler')
+const { kaderisasiSdmHandler } = require('../handler/postKaderisasiSdmHandler')
+const { getAbsentSdm } = require('../handler/getAbsentSdm')
+const { imageViewHandler } = require('../handler/imageViewHandler')
+const { getBuktiAbsensiSdmHandler } = require('../handler/getBuktiAbsensiSdmHandler')
 
 router.get('/', (req, res) => {
   res.render('homePage')
@@ -11,37 +15,31 @@ router.get('/', (req, res) => {
 
 router.get('/tentang', (req, res) => {
   res.render('underDevelopment')
-}).all('/tentang', (req, res) => {
-  res.render('methodUnsupported')
 })
 
 router.get('/absensi', getAbsentHandler)
   .post('/absensi', postAbsentHandler)
-  .all('/absensi', (req, res) => {
-    res.status(403).render('methodUnsupported')
-  })
 
 router.get('/tahap-pengembangan', (req, res) => {
   res.render('underDevelopment')
-}).all('/tahap-pengembangan', (req, res) => {
-  res.render('methodUnsupported')
 })
 
 router.get('/kontak', (req, res) => {
   res.render('underDevelopment')
-}).all('/kontak', (req, res) => {
-  res.render('methodUnsupported')
 })
 
 router.get('/login', (req, res) => {
   res.render('underDevelopment')
-}).all('/login', (req, res) => {
-  res.render('methodUnsupported')
 })
 
-router.get('/hei', async (req, res) => {
-  res.send(req.hostname)
-})
+router.post('/kaderisasi/sdm', kaderisasiSdmHandler)
+
+router.get('/kaderisasi/sdm/absensi', getAbsentSdm)
+  .post('/kaderisasi/sdm/absensi', uploadAbsentSdmHandler)
+
+router.get('/kaderisasi/sdm/absensi/bukti', getBuktiAbsensiSdmHandler)
+
+router.get('/images/view/:imageId', imageViewHandler)
 
 router.all('*', (req, res) => {
   res.end('maaf, halaman yang anda cari tidak ditemukan, atau metode tidak didukung:)')
