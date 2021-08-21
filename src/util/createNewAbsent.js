94aa1d0af91832f735e7f505dd783034b97ac192
luckyAkbar <luckyakbar1509@gmail.com>
2021-08-21 20:14:27 +0700

    adding new divisi field to be queried, change csv file to be base data, rework query string initialization, and add new createNewKegiatan function and exporting it

--- src/util/createNewAbsent.js
+++ src/util/createNewAbsent.js
@@ -3,16 +3,17 @@ const { readDataCsv } = require('./readDataCsv')
 
 const table_name = 'absensi'
 const refIdField = 'referensi_id'
+const divField = 'divisi'
 const npmField = 'npm'
 const namaField = 'nama'
 
 const initNewAbsentRecord = async (res, referensiId) => {
-  let result = readDataCsv(__dirname+'/../../db/data/test.csv')
+  let result = readDataCsv(__dirname+'/../../db/data/fullData.csv')
 
   try {
     for(let i = 0; i < result.length; i++) {
-      const query = `INSERT INTO ${table_name} (${refIdField}, ${npmField}, ${namaField}) VALUES ($1, $2, $3)`
-      const params = [`${referensiId}`, `${result[i].npm}`, `${result[i].nama}`]
+      const query = `INSERT INTO ${table_name} (${refIdField}, ${npmField}, ${namaField}, ${divField}) VALUES ($1, $2, $3, $4)`
+      const params = [`${referensiId}`, `${result[i].npm}`, `${result[i].nama}`, `${result[i].divisi}`]
 
       const finished = await testQuery(query, params)
     }
@@ -32,4 +33,35 @@ const createNewAbsent = async (res, referensiId) => {
   }
 }
 
-module.exports = { createNewAbsent }
+const createNewKegiatan = async (refId, {
+  namaKegiatan,
+  tanggalPelaksanaan,
+  tanggalBerakhir
+}) => {
+  const query = `INSERT INTO kegiatan (
+    kegiatan_id,
+    nama_kegiatan,
+    tanggal_pelaksanaan,
+    tanggal_berakhir
+  ) VALUES ($1, $2, $3, $4)`
+
+  const params = [
+    refId,
+    namaKegiatan,
+    tanggalPelaksanaan,
+    tanggalBerakhir
+  ]
+
+  try {
+    await testQuery(query, params)
+    return
+  } catch (e) {
+    console.log(e)
+    throw new Error('Failed to create new kegiatan')
+  }
+}
+
+module.exports = {
+  createNewAbsent,
+  createNewKegiatan
+}
