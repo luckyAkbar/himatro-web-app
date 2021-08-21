e08d1fd28690e4f45e95b3e5ca54ed110de3b8f4
luckyAkbar <luckyakbar1509@gmail.com>
2021-08-21 20:23:12 +0700

    to make capitalize each first letter in word (unused for now)

---
+++ src/util/namaFormatter.js
@@ -0,0 +1,14 @@
+const namaFormatter = (nama) => {
+  nama = nama.toLowerCase()
+  nama = nama.split(' ')
+
+  for (let i = 0; i < nama.length; i++) {
+    nama[i] = nama[i][0].toUpperCase() + nama[i].substr(1);
+  }
+
+  nama = nama.join(" ")
+  console.log(nama);
+  return nama
+}
+
+module.exports = { namaFormatter }
