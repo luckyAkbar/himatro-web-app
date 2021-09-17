47a40ce1f63621067afa6a5e1430858823b2e90b
luckyAkbar <luckyakbar1509@gmail.com>
2021-09-17 17:42:53 +0700

    improving guard function by jwt checking, if jwt not present, redirecting to login page

--- src/middleware/authentication.js
+++ src/middleware/authentication.js
@@ -27,48 +27,43 @@ const authentication = async (req, res, next) => {
         console.log('error clearing expired session', e)
     }
 
-    if (jwt) {
-        const query = 'SELECT * FROM sessions WHERE sessionid = $1'
-        try {    
-            const { sessionId, session, exp, email } = verifyJWTToken(jwt)
-            
-            if (getSecondsAfterEpoch() > exp) {
-                console.log('token expired')
-                res.status(403).redirect('/login')
-                return
-            }
+    if (!jwt) {
+        res.status(403).redirect('/login')
+        return
+    }
 
-            const params = [sessionId]
-
-            const { rowCount, rows } = await testQuery(query, params)
-
-            if (rowCount === 0) {
-                res.status(403).render('errorPage', {
-                    errorMessage: 'Invalid session issued.'
-                })
-                return
-            }
-
-            if (session !== rows[0].session && req.headers['user-agent'] !== rows[0].useragent) {
-                res.status(403).render('errorPage', {
-                    errorMessage: 'Unauthorized token usage. Please login first.'
-                })
-                return
-            }
-
-            req.email = email
-            next()
-            return
-
-        } catch(e) {
-            console.log(chalk.white.bold.bgRed(`Unothorized access detected on ${req.path}`))
+    const query = 'SELECT * FROM sessions WHERE sessionid = $1'
+    try {    
+        const { sessionId, session, exp, email } = verifyJWTToken(jwt)
+        
+        if (getSecondsAfterEpoch() > exp) {
             res.status(403).redirect('/login')
             return
         }
-    }
 
-    res.status(403).redirect('/login')
-    return
+        const params = [sessionId]
+
+        const { rowCount, rows } = await testQuery(query, params)
+
+        if (rowCount === 0) {
+            res.status(403).json({ errorMessage: 'Invalid session issued. Please login first' })
+            return
+        }
+
+        if (session !== rows[0].session && req.headers['user-agent'] !== rows[0].useragent) {
+            res.status(403).json({ errorMessage: 'Unauthorized token usage. Please login first.' })
+            return
+        }
+
+        req.email = email
+        next()
+        return
+
+    } catch(e) {
+        console.log(chalk.white.bold.bgRed(`Unothorized access detected on ${req.path}`))
+        res.status(403).redirect('/login')
+        return
+    }
 }
 
 const clearExpiredSession = async () => {
