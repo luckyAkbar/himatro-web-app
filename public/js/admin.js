68b3dc533d0e0c9316a577abfe4554700985c27c
luckyAkbar <luckyakbar1509@gmail.com>
2021-09-28 08:55:22 +0700

    change target endpoint and make it flexible without adding server domain (relative URL)

--- public/js/admin.js
+++ public/js/admin.js
@@ -125,7 +125,7 @@ buttonAbsentPengurus.addEventListener('click', async () => {
     jamBerakhir: formAbsenPengurus.jamBerakhir.value,
   };
 
-  const targetEndpoint = 'http://farzani.me/feature/feature001';
+  const targetEndpoint = '/feature/feature001';
   try {
     const response = await postData(payload, targetEndpoint);
 
