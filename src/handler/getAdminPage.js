22a77da0b24f0184be6c53c86af393dfe6512214
luckyAkbar <luckyakbar1509@gmail.com>
2021-10-06 07:08:28 +0700

    add get admin page in database, and use it to compare user permission to see admin page

--- src/handler/getAdminPage.js
+++ src/handler/getAdminPage.js
@@ -1,11 +1,13 @@
+const { getMinimumFeaturePermission } = require('../util/getMinimumFeaturePermission');
 const { getUpcomingEvents } = require('../util/getUpcomingEvents');
 const { getUserPermissionLevel } = require('../util/getUserPermissionLevel');
 
 const getAdminPage = async (req, res) => {
   try {
     const userPermissionLevel = await getUserPermissionLevel(req.email);
+    const featurePermissonLevel = await getMinimumFeaturePermission('feature005');
 
-    if (userPermissionLevel < 31) {
+    if (userPermissionLevel < featurePermissonLevel) {
       console.log(`${req.email} trying to access ${req.originalUrl}`);
       res.status(403).render('errorPage', {
         errorMessage: 'FORBIDDEN. You are not allowed to see this page.',
