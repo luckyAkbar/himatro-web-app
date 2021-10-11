1a61609231b3df8a30020fb96958f74bd50db36a
luckyAkbar <luckyakbar1509@gmail.com>
2021-10-11 23:04:32 +0700

    implement morgan as a new logger to console

--- src/middleware/incomingRequestLogger.js
+++ src/middleware/incomingRequestLogger.js
@@ -1,19 +1,43 @@
-const chalk = require('chalk');
+const morgan = require('morgan');
 
-const incomingRequestLogger = (req, res, next) => {
-  const time = Date();
-  console.log(chalk.bold('INCOMING REQUEST'));
-  console.log(
-    req.protocol,
-    req.method,
-    req.originalUrl,
-    time.toString().slice(0, 33),
-    req.body,
-    req.params,
-    req.query,
-  );
-  console.log(chalk.bold('END'));
-  next();
-};
+morgan.token('body', (req, res) => {
+  return JSON.stringify(req.body);
+});
+
+morgan.token('query', (req, res) => {
+  return JSON.stringify(req.query);
+});
+
+morgan.token('params', (req, res) => {
+  return JSON.stringify(req.params);
+});
+
+morgan.token('splitter', (req) => {
+  return "\x1b[36m----------------------------------------------------------------------------------\x1b[0m\n";
+});
+
+morgan.token('statusColor', (req, res) => {
+  const status = (typeof res.headersSent !== 'boolean' ? Boolean(res.header) : res.headersSent) ?
+    res.statusCode: undefined;
+
+  // get status color
+  const color = status >= 500 ? 31 // red
+    : status >= 400 ? 33 // yellow
+    : status >= 300 ? 36 // cyan
+    : status >= 200 ? 32 // green
+    : 0; // no color
+
+  return '\x1b[' + color + 'm' + status + '\x1b[0m';
+});
+
+const incomingRequestLogger = morgan(`:splitter\x1b[33m:method\x1b[0m \x1b[36m:url\x1b[0m :statusColor \x1b[1m:response-time ms || :date[web]\x1b[0m
+  length|:res[content-length]
+  \x1b[2mbody:body\x1b[0m
+  \x1b[2mquery:query\x1b[0m
+  \x1b[2mparams:params\x1b[0m
+  :referrer
+  :remote-addr
+  :user-agent
+`);
 
 module.exports = { incomingRequestLogger };
