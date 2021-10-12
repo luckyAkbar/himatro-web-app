d8227df6c646be57a311ad0f57f9192b574028ae
luckyAkbar <luckyakbar1509@gmail.com>
2021-10-12 09:58:51 +0700

    adding protocol to logger

--- src/middleware/incomingRequestLogger.js
+++ src/middleware/incomingRequestLogger.js
@@ -1,14 +1,11 @@
 const morgan = require('morgan');
 
-morgan.token('urlOnly', (req) => req.path);
-
-morgan.token('body', (req) => JSON.stringify(req.body));
-
-morgan.token('query', (req) => JSON.stringify(req.query));
-
-morgan.token('params', (req) => JSON.stringify(req.params));
-
 morgan.token('splitter', () => '\x1b[36m----------------------------------------------------------------------------------\x1b[0m\n');
+morgan.token('urlOnly', (req) => req.path);
+morgan.token('body', (req) => JSON.stringify(req.body));
+morgan.token('query', (req) => JSON.stringify(req.query));
+morgan.token('params', (req) => JSON.stringify(req.params));
+morgan.token('protocol', (req, res) => req.protocol);
 
 morgan.token('statusColor', (req, res) => {
   const status = (typeof res.headersSent !== 'boolean' ? Boolean(res.header) : res.headersSent) ? res.statusCode : undefined;
@@ -23,7 +20,7 @@ morgan.token('statusColor', (req, res) => {
   return `\x1b[${color}m${status}\x1b[0m`;
 });
 
-const incomingRequestLogger = morgan(`:splitter\x1b[33m:method\x1b[0m \x1b[36m:urlOnly\x1b[0m :statusColor \x1b[1m:response-time ms || :date[web]\x1b[0m
+const incomingRequestLogger = morgan(`:splitter\x1b[1m:protocol\x1b[0m \x1b[33m:method\x1b[0m \x1b[36m:urlOnly\x1b[0m :statusColor \x1b[1m:response-time ms || :date[web]\x1b[0m
   length|:res[content-length]
   \x1b[2mparams:params\x1b[0m
   \x1b[2mquery:query\x1b[0m
