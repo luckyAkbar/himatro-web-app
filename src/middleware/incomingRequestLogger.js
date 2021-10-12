6b1e858725e55e9059ff6fff3257ca0763397839
luckyAkbar <luckyakbar1509@gmail.com>
2021-10-12 09:38:41 +0700

    fix logging middlerware in parms, body and query output

--- src/middleware/incomingRequestLogger.js
+++ src/middleware/incomingRequestLogger.js
@@ -11,24 +11,23 @@ morgan.token('params', (req) => JSON.stringify(req.params));
 morgan.token('splitter', () => '\x1b[36m----------------------------------------------------------------------------------\x1b[0m\n');
 
 morgan.token('statusColor', (req, res) => {
-  const status = (typeof res.headersSent !== 'boolean' ? Boolean(res.header) : res.headersSent) ?
-    res.statusCode: undefined;
+  const status = (typeof res.headersSent !== 'boolean' ? Boolean(res.header) : res.headersSent) ? res.statusCode : undefined;
 
   // get status color
-  const color = status >= 500 ? 31 // red
-    : status >= 400 ? 33 // yellow
-    : status >= 300 ? 36 // cyan
-    : status >= 200 ? 32 // green
-    : 0; // no color
+  const color = status >= 500 // red
+    ? 31 : status >= 400 // yellow
+      ? 33 : status >= 300 // cyan
+        ? 36 : status >= 200 // green
+          ? 32 : 0; // no color
 
-  return '\x1b[' + color + 'm' + status + '\x1b[0m';
+  return `\x1b[${color}m${status}\x1b[0m`;
 });
 
 const incomingRequestLogger = morgan(`:splitter\x1b[33m:method\x1b[0m \x1b[36m:urlOnly\x1b[0m :statusColor \x1b[1m:response-time ms || :date[web]\x1b[0m
   length|:res[content-length]
-  \x1b[2mbody:body\x1b[0m
-  \x1b[2mquery:query\x1b[0m
   \x1b[2mparams:params\x1b[0m
+  \x1b[2mquery:query\x1b[0m
+  \x1b[2mbody:body\x1b[0m
   :referrer
   :remote-addr
   :user-agent
