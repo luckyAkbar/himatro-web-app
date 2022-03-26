d65e706c6869d50744153fff8c65af4f2af9e405
luckyAkbar <luckyakbar1509@gmail.com>
2022-03-26 09:15:15 +0700

    test

--- src/util/newAbsentFiller.js
+++ src/util/newAbsentFiller.js
@@ -1,49 +1,49 @@
-const { CustomError } = require('../classes/CustomError');
-const { testQuery } = require('../../db/connection');
+const { CustomError } = require("../classes/CustomError");
+const { testQuery } = require("../../db/connection");
 
-const {
-  formatDateToStandartFormat,
-  getTimeStamp,
-} = require('./getTimeStamp');
+const { formatDateToStandartFormat, getTimeStamp } = require("./getTimeStamp");
 
 const {
   namaValidator,
   NPMValidator,
   keteranganAbsentValidator,
-} = require('./newValidator');
+} = require("./newValidator");
 
 const insertKehadiranRecord = async (queryParams, bodyRequest) => {
   const { absentId } = queryParams;
   const waktuPengisian = getTimeStamp();
-  const {
-    npm,
-    keterangan,
-    alasan = null,
-  } = bodyRequest;
-  const query = 'UPDATE absensi SET keterangan = $1, alasan_izin = $2, waktu_pengisian = $3 WHERE npm = $4 AND referensi_id = $5';
+  const { npm, keterangan, alasan = null } = bodyRequest;
+  const query =
+    "UPDATE absensi SET keterangan = $1, alasan_izin = $2, waktu_pengisian = $3 WHERE npm = $4 AND referensi_id = $5";
   const params = [keterangan, alasan, waktuPengisian, npm, absentId];
 
   try {
     await testQuery(query, params);
   } catch (e) {
-    throw new CustomError('Maaf, server gagal memasukan data anda ke dalam daftar absensi.', 500);
+    throw new CustomError(
+      "Maaf, server gagal memasukan data anda ke dalam daftar absensi.",
+      500
+    );
   }
 };
 
 const checkIfUserAlreadyFilledAbsentForm = async (NPM, absentId) => {
-  const query = 'SELECT keterangan FROM absensi WHERE npm = $1 AND referensi_id = $2';
+  const query =
+    "SELECT keterangan FROM absensi WHERE npm = $1 AND referensi_id = $2";
   const params = [NPM, absentId];
 
   try {
     const { rows } = await testQuery(query, params);
     if (rows[0].keterangan !== null) throw new Error();
   } catch (e) {
-    throw new CustomError('Anda sudah mengisi form ini, dan tidak bisa mengubahnya.');
+    throw new CustomError(
+      "Anda sudah mengisi form ini, dan tidak bisa mengubahnya."
+    );
   }
 };
 
 const getFormAbsentDetails = async (absentId) => {
-  const query = 'SELECT * FROM kegiatan WHERE kegiatan_id = $1';
+  const query = "SELECT * FROM kegiatan WHERE kegiatan_id = $1";
   const params = [absentId];
 
   try {
@@ -57,21 +57,33 @@ const getFormAbsentDetails = async (absentId) => {
       tanggalBerakhir: rows[0].tanggal_berakhir,
     };
   } catch (e) {
-    throw new CustomError('Absent form not found.', 404);
+    throw new CustomError("Absent form not found.", 404);
   }
 };
 
-const validateAbsentFormWriteableStatusFromDate = ({ tanggalPelaksanaan, tanggalBerakhir, namaKegiatan }) => {
+const validateAbsentFormWriteableStatusFromDate = ({
+  tanggalPelaksanaan,
+  tanggalBerakhir,
+  namaKegiatan,
+}) => {
   const now = new Date();
-  const stillClosedErrorMsg = `Form absen ${namaKegiatan} akan dibuka pada ${formatDateToStandartFormat(tanggalPelaksanaan)}`;
-  const alreadyClosedErrorMsg = `Form absen untuk ${namaKegiatan} sudah ditutup pada ${formatDateToStandartFormat(tanggalBerakhir)}`;
+  const stillClosedErrorMsg = `Form absen ${namaKegiatan} akan dibuka pada ${formatDateToStandartFormat(
+    tanggalPelaksanaan
+  )}`;
+  const alreadyClosedErrorMsg = `Form absen untuk ${namaKegiatan} sudah ditutup pada ${formatDateToStandartFormat(
+    tanggalBerakhir
+  )}`;
+
+  console.log("ini target noiw", now);
+  console.log(tanggalPelaksanaan);
+  console.log(tanggalBerakhir);
 
   if (tanggalPelaksanaan > now) throw new CustomError(stillClosedErrorMsg);
   if (tanggalBerakhir < now) throw new CustomError(alreadyClosedErrorMsg);
 };
 
 const checkIsNPMExistsOnPengurusTable = async (NPM) => {
-  const query = 'SELECT * FROM pengurus WHERE npm = $1';
+  const query = "SELECT * FROM pengurus WHERE npm = $1";
   const params = [NPM];
 
   try {
@@ -79,12 +91,15 @@ const checkIsNPMExistsOnPengurusTable = async (NPM) => {
 
     if (rowCount === 0) throw new Error();
   } catch (e) {
-    throw new CustomError('Maaf, NPM anda tidak ditemukan sebagai pengurus Himatro.', 404);
+    throw new CustomError(
+      "Maaf, NPM anda tidak ditemukan sebagai pengurus Himatro.",
+      404
+    );
   }
 };
 
 const checkIsNPMExistsOnAbsentEntry = async (absentId, NPM) => {
-  const query = 'SELECT nama FROM absensi WHERE npm = $1 and referensi_id = $2';
+  const query = "SELECT nama FROM absensi WHERE npm = $1 and referensi_id = $2";
   const params = [NPM, absentId];
 
   try {
@@ -92,17 +107,15 @@ const checkIsNPMExistsOnAbsentEntry = async (absentId, NPM) => {
 
     if (rowCount === 0) throw new Error();
   } catch (e) {
-    throw new CustomError('Maaf, NPM anda tidak ditemukan di daftar absensi. Mohon periksa NPM anda.', 404);
+    throw new CustomError(
+      "Maaf, NPM anda tidak ditemukan di daftar absensi. Mohon periksa NPM anda.",
+      404
+    );
   }
 };
 
 const validateAbsentFormInputData = async (bodyRequest, absentId) => {
-  const {
-    nama,
-    npm,
-    keterangan,
-    alasan,
-  } = bodyRequest;
+  const { nama, npm, keterangan, alasan } = bodyRequest;
 
   try {
     namaValidator(nama);
